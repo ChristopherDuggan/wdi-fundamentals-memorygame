f56554b0028a02158473a8b42688a6afba7c367e
Christopher Duggan <christopherduggan@gmail.com>
2019-03-17 19:38:38 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,14 @@
-const cardOne = 'queen';
-const cardTwo = 'queen';
-const cardThree = 'king';
-const cardFour = 'king';
+const cards = ['queen', 'queen', 'king', 'king'];
+let cardsInPlay = [];
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
+let cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2) {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert('You found a match!');
+  } else {
+    alert('Sorry, try again.')
+  }
+}
